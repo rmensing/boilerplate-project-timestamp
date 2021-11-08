28096b0a70412cc0a78ada30f0080a0a6a1beea7
Robert Mensing <rmensing@gmail.com>
2021-11-08 03:25:32 +0000

    project complete

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app = express();
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -25,6 +29,33 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get('/api/:input?', (req, res, next) => {
+  var input = req.params.input;
+  console.log("First input: " + input);
+  var date = new Date(input);
+  console.log("First date: " + date);
+  if (isNumeric(input)){
+    date = new Date(parseInt(input)*1000);
+    console.log("isNumeric: " + date);
+  }
+  if (input ==null){
+    date = new Date();
+    console.log("test null: " + date);
+  }
+  console.log(date);
+  req.dStr = date.toGMTString();
+  req.uts = Date.parse(date);
+  console.log(date.toGMTString());
+  console.log(Date.parse(date));
+  next();
+},
+(req, res) => {
+  if (req.dStr === "Invalid Date") {
+    res.json({error: req.dStr});
+  } else {
+  res.json({unix: req.uts, utc: req.dStr});
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
